61881cb6ef11a1f665a0a86c9f41c46608d50406
suvinayseth <suvinay.seth@housing.com>
2015-06-17 04:23:49 +0530

    Lesson 2 learnings

--- Movie Quote/ViewController.swift
+++ Movie Quote/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    @IBOutlet weak var recordStop: UIButton!
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        recordingLabel.hidden = true
+        recordStop.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBAction func recordAudio(sender: UIButton) {
+    
+        if recordingLabel.hidden == true {
+            recordingLabel.hidden = false
+            recordStop.hidden = false
+        }
+    }
 
+    @IBAction func recordStop(sender: UIButton) {
+        recordStop.hidden = true
+        recordingLabel.hidden = true
+    }
 }
 
